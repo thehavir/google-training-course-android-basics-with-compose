2828d241ddb01518c6c4be277911b5a31c6002ed
havir <havir.valizadeh@klar.mx>
2026-05-04 07:09:24 +0200

    Implement birthday card

--- Unit1Project1BirthdayCard/app/src/main/java/dev/havir/unit1_project1_birthdaycard/MainActivity.kt
+++ Unit1Project1BirthdayCard/app/src/main/java/dev/havir/unit1_project1_birthdaycard/MainActivity.kt
@@ -4,13 +4,23 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import dev.havir.unit1_project1_birthdaycard.ui.theme.Unit1Project1BirthdayCardTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,29 +29,62 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             Unit1Project1BirthdayCardTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                HappyBirthdayApp()
             }
         }
     }
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun HappyBirthdayApp(modifier: Modifier = Modifier) {
+    Box(
+        modifier = modifier.fillMaxSize()
+    ) {
+        Image(
+            painter = painterResource(R.drawable.androidparty),
+            contentDescription = null,
+            alpha = 0.5f,
+            contentScale = ContentScale.Crop
+        )
+        GreetingText(
+            message = stringResource(R.string.happy_birthday_text),
+            from = stringResource(R.string.signature_text),
+            modifier = Modifier.align(
+                Alignment.Center
+            )
+        )
+    }
+}
+
+@Composable
+fun GreetingText(
+    message: String,
+    from: String,
+    modifier: Modifier = Modifier,
+) {
+    Column(modifier = modifier) {
+        Text(
+            message,
+            textAlign = TextAlign.Center,
+            fontSize = 100.sp,
+            lineHeight = 116.sp,
+            modifier = Modifier.fillMaxWidth()
+        )
+        Text(
+            from,
+            textAlign = TextAlign.Center,
+            fontSize = 36.sp,
+            modifier = Modifier
+                .padding(16.dp)
+                .fillMaxWidth()
+        )
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
+fun HappyBirthdayAppPreview() {
     Unit1Project1BirthdayCardTheme {
-        Greeting("Android")
+        HappyBirthdayApp()
     }
 }
