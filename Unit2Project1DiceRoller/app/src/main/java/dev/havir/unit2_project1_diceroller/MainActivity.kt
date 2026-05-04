7e75e2529e17a274d656d4bd538cfd831d05bdd3
havir <havir.valizadeh@klar.mx>
2026-05-04 17:54:46 +0200

    Unit 2 - Project 1 - Dice Roller - implement

--- Unit2Project1DiceRoller/app/src/main/java/dev/havir/unit2_project1_diceroller/MainActivity.kt
+++ Unit2Project1DiceRoller/app/src/main/java/dev/havir/unit2_project1_diceroller/MainActivity.kt
@@ -4,13 +4,25 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import dev.havir.unit2_project1_diceroller.ui.theme.Unit2Project1DiceRollerTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,29 +31,56 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             Unit2Project1DiceRollerTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                DiceRollerApp()
             }
         }
     }
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
+fun DiceRollerApp() {
+    var result by rememberSaveable { mutableIntStateOf(1) }
+    val onRoll = { result = (1..6).random() }
+    DiceWithButtonAndImage(
+        modifier = Modifier
+            .fillMaxSize()
+            .wrapContentSize(Alignment.Center),
+        result = result,
+        onRoll = onRoll,
     )
+}
+
+@Composable
+fun DiceWithButtonAndImage(
+    modifier: Modifier = Modifier, result: Int, onRoll: () -> Unit
+) {
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = modifier,
+    ) {
+        val imageResource = when (result) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+        }
+        Image(
+            painterResource(imageResource),
+            contentDescription = result.toString()
+        )
+        Spacer(modifier = Modifier.height(16.dp))
+        Button(onClick = onRoll) {
+            Text(stringResource(R.string.roll))
+        }
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
+fun DiceRollerAppPreview() {
     Unit2Project1DiceRollerTheme {
-        Greeting("Android")
+        DiceRollerApp()
     }
 }
