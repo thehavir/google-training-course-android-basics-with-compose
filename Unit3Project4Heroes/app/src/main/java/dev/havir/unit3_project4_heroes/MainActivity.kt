2b2ead17d1cc5212c6ebc0942551c2b192e784b3
havir <havir.valizadeh@klar.mx>
2026-05-08 20:25:49 +0200

    Unit 3 - Project 4 - Heroes - Init

---
+++ Unit3Project4Heroes/app/src/main/java/dev/havir/unit3_project4_heroes/MainActivity.kt
@@ -0,0 +1,47 @@
+package dev.havir.unit3_project4_heroes
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import dev.havir.unit3_project4_heroes.ui.theme.Unit3Project4HeroesTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            Unit3Project4HeroesTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    Unit3Project4HeroesTheme {
+        Greeting("Android")
+    }
+}
